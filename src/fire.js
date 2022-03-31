da66361fb91ea2a623bdd4dead2956332c946b46
Andrei Cozma <andreicozma89@yahoo.com>
2022-03-31 16:44:44 +0100

    Update fire.js

--- src/fire.js
+++ src/fire.js
@@ -1,12 +1,19 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyATBUQ4GJzqx6QIdUg0pJIfeZ3LmwF_P-4",
-  authDomain: "login2-4aa15.firebaseapp.com",
-  projectId: "login2-4aa15",
-  storageBucket: "login2-4aa15.appspot.com",
-  messagingSenderId: "852514390530",
-  appId: "1:852514390530:web:28f43f5d92a148defc9d1b",
+  
+  apiKey: "AIzaSyAD87WRh-7x5_llgB8r9QrUhw-t9CYfvv4",
+
+  authDomain: "login3-c2619.firebaseapp.com",
+
+  projectId: "login3-c2619",
+
+  storageBucket: "login3-c2619.appspot.com",
+
+  messagingSenderId: "907682653031",
+
+  appId: "1:907682653031:web:0c5c7318f5f7e8ba8145c2"
+
 };
 
 const fire = firebase.initializeApp(firebaseConfig);
